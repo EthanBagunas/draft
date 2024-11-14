7616719768d008dd60d9cbd33f0b72fec30f8691
oddaudd <audreiamanence@gmail.com>
2024-11-14 16:19:10 +0800

    gwapa ko so much
    naa ray gitry

---
+++ ASI.Basecode.WebApp/wwwroot/js/modal.js
@@ -0,0 +1,18 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+
